993c892222652698323c764b534697b5b9297a53
Athacyr87 <souza.87.ferreira@gmail.com>
2026-05-06 21:25:41 -0300

    Adicionar arquivos de projeto.

---
+++ visualizadorTGA/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace visualizadorTGA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirTgaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ferramentasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.centralizarImagemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelImagem = new System.Windows.Forms.Panel();
+            this.pictureBoxImagem = new System.Windows.Forms.PictureBox();
+            this.labelZoom = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panelImagem.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.ferramentasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(960, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirTgaToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // abrirTgaToolStripMenuItem
+            // 
+            this.abrirTgaToolStripMenuItem.Name = "abrirTgaToolStripMenuItem";
+            this.abrirTgaToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.abrirTgaToolStripMenuItem.Text = "Abrir TGA...";
+            this.abrirTgaToolStripMenuItem.Click += new System.EventHandler(this.abrirTgaToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // ferramentasToolStripMenuItem
+            // 
+            this.ferramentasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.centralizarImagemToolStripMenuItem});
+            this.ferramentasToolStripMenuItem.Name = "ferramentasToolStripMenuItem";
+            this.ferramentasToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.ferramentasToolStripMenuItem.Text = "Ferramentas";
+            // 
+            // centralizarImagemToolStripMenuItem
+            // 
+            this.centralizarImagemToolStripMenuItem.CheckOnClick = true;
+            this.centralizarImagemToolStripMenuItem.Name = "centralizarImagemToolStripMenuItem";
+            this.centralizarImagemToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.centralizarImagemToolStripMenuItem.Text = "Centralizar imagem";
+            this.centralizarImagemToolStripMenuItem.CheckedChanged += new System.EventHandler(this.centralizarImagemToolStripMenuItem_CheckedChanged);
+            // 
+            // panelImagem
+            // 
+            this.panelImagem.AutoScroll = true;
+            this.panelImagem.BackColor = System.Drawing.Color.White;
+            this.panelImagem.Controls.Add(this.pictureBoxImagem);
+            this.panelImagem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelImagem.Location = new System.Drawing.Point(0, 24);
+            this.panelImagem.Name = "panelImagem";
+            this.panelImagem.Size = new System.Drawing.Size(960, 576);
+            this.panelImagem.TabIndex = 1;
+            this.panelImagem.Resize += new System.EventHandler(this.panelImagem_Resize);
+            // 
+            // pictureBoxImagem
+            // 
+            this.pictureBoxImagem.BackColor = System.Drawing.Color.White;
+            this.pictureBoxImagem.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxImagem.Name = "pictureBoxImagem";
+            this.pictureBoxImagem.Size = new System.Drawing.Size(1, 1);
+            this.pictureBoxImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImagem.TabIndex = 0;
+            this.pictureBoxImagem.TabStop = true;
+            // 
+            // labelZoom
+            // 
+            this.labelZoom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelZoom.AutoSize = true;
+            this.labelZoom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.labelZoom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelZoom.ForeColor = System.Drawing.Color.White;
+            this.labelZoom.Location = new System.Drawing.Point(899, 572);
+            this.labelZoom.Name = "labelZoom";
+            this.labelZoom.Padding = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.labelZoom.Size = new System.Drawing.Size(49, 23);
+            this.labelZoom.TabIndex = 2;
+            this.labelZoom.Text = "100%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 600);
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.labelZoom);
+            this.Controls.Add(this.panelImagem);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Visualizador TGA";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panelImagem.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirTgaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ferramentasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem centralizarImagemToolStripMenuItem;
+        private System.Windows.Forms.Panel panelImagem;
+        private System.Windows.Forms.PictureBox pictureBoxImagem;
+        private System.Windows.Forms.Label labelZoom;
+    }
+}
---
+++ visualizadorTGA/Form1.cs
@@ -0,0 +1,450 @@
+﻿using SixLabors.ImageSharp.Formats.Bmp;
+using ImageSharpImage = SixLabors.ImageSharp.Image;
+
+namespace visualizadorTGA
+{
+    public partial class Form1 : Form
+    {
+        private const float ZoomPasso = 1.10f;
+        private const float ZoomMinimo = 0.05f;
+        private const float ZoomMaximo = 20f;
+        private static readonly string[] ExtensoesNavegaveis =
+        {
+            ".tga", ".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tif", ".tiff", ".webp"
+        };
+
+        private float zoomAtual = 1f;
+        private List<string> imagensDaPasta = new();
+        private int indiceImagemAtual = -1;
+        private bool centralizarImagemAtiva;
+        private ContextMenuStrip? menuContextoImagem;
+        private ToolStripMenuItem? menuContextoZoom100;
+        private ToolStripMenuItem? menuContextoRotacionar;
+        private ToolStripMenuItem? menuContextoCantoSuperiorEsquerdo;
+        private ToolStripMenuItem? menuContextoCentralizar;
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+
+            pictureBoxImagem.MouseWheel += areaImagem_MouseWheel;
+            panelImagem.MouseWheel += areaImagem_MouseWheel;
+            pictureBoxImagem.MouseEnter += areaImagem_MouseEnter;
+            panelImagem.MouseEnter += areaImagem_MouseEnter;
+
+            ConfigurarMenuContextoImagem();
+            AtualizarIndicadorZoom();
+            labelZoom.BringToFront();
+        }
+
+        private void abrirTgaToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            using var dialog = new OpenFileDialog
+            {
+                Title = "Abrir imagem TGA",
+                Filter = "Imagens TGA (*.tga)|*.tga|Todos os arquivos (*.*)|*.*",
+                Multiselect = false,
+                CheckFileExists = true
+            };
+
+            if (dialog.ShowDialog(this) != DialogResult.OK)
+            {
+                return;
+            }
+
+            CarregarImagemTga(dialog.FileName);
+        }
+
+        private void sairToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void CarregarImagemTga(string caminhoArquivo)
+        {
+            try
+            {
+                using ImageSharpImage imagem = ImageSharpImage.Load(caminhoArquivo);
+                using var stream = new MemoryStream();
+                imagem.Save(stream, new BmpEncoder());
+                stream.Position = 0;
+
+                using var bitmapTemp = new Bitmap(stream);
+                var bitmapFinal = new Bitmap(bitmapTemp);
+
+                AtualizarImagem(bitmapFinal);
+                AtualizarListaNavegacao(caminhoArquivo);
+                Text = $"Visualizador TGA - {Path.GetFileName(caminhoArquivo)}";
+            }
+            catch (SixLabors.ImageSharp.UnknownImageFormatException)
+            {
+                MessageBox.Show(
+                    this,
+                    "Nao foi possivel reconhecer este arquivo como imagem valida.",
+                    "Formato invalido",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    this,
+                    $"Erro ao abrir a imagem:\n{ex.Message}",
+                    "Falha ao abrir arquivo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void AtualizarImagem(Bitmap novaImagem)
+        {
+            var imagemAnterior = pictureBoxImagem.Image;
+            pictureBoxImagem.Image = novaImagem;
+            imagemAnterior?.Dispose();
+
+            zoomAtual = CalcularZoomInicial(novaImagem);
+            RedimensionarImagemExibida(resetarScroll: true);
+            AtualizarIndicadorZoom();
+        }
+
+        private float CalcularZoomInicial(System.Drawing.Image imagem)
+        {
+            if (panelImagem.ClientSize.Width <= 0 || panelImagem.ClientSize.Height <= 0)
+            {
+                return 1f;
+            }
+
+            float zoomHorizontal = panelImagem.ClientSize.Width / (float)imagem.Width;
+            float zoomVertical = panelImagem.ClientSize.Height / (float)imagem.Height;
+            float zoomParaCaber = Math.Min(zoomHorizontal, zoomVertical);
+
+            return Math.Clamp(Math.Min(1f, zoomParaCaber), ZoomMinimo, ZoomMaximo);
+        }
+
+        private void areaImagem_MouseEnter(object? sender, EventArgs e)
+        {
+            pictureBoxImagem.Focus();
+        }
+
+        private void areaImagem_MouseWheel(object? sender, MouseEventArgs e)
+        {
+            if (pictureBoxImagem.Image is null)
+            {
+                return;
+            }
+
+            Point pontoPainel = panelImagem.PointToClient(Cursor.Position);
+            if (!panelImagem.ClientRectangle.Contains(pontoPainel))
+            {
+                return;
+            }
+
+            float passos = e.Delta / 120f;
+            if (passos == 0f)
+            {
+                return;
+            }
+
+            float fatorZoom = (float)Math.Pow(ZoomPasso, passos);
+            AjustarZoom(zoomAtual * fatorZoom, pontoPainel);
+        }
+
+        private void AjustarZoom(float novoZoom, Point pontoAncoragemPainel)
+        {
+            if (pictureBoxImagem.Image is null)
+            {
+                return;
+            }
+
+            novoZoom = Math.Clamp(novoZoom, ZoomMinimo, ZoomMaximo);
+            if (Math.Abs(novoZoom - zoomAtual) < 0.0001f)
+            {
+                return;
+            }
+
+            int deslocamentoAtualX = panelImagem.AutoScroll ? -panelImagem.AutoScrollPosition.X : 0;
+            int deslocamentoAtualY = panelImagem.AutoScroll ? -panelImagem.AutoScrollPosition.Y : 0;
+
+            float pontoImagemX = (pontoAncoragemPainel.X + deslocamentoAtualX - pictureBoxImagem.Left) / zoomAtual;
+            float pontoImagemY = (pontoAncoragemPainel.Y + deslocamentoAtualY - pictureBoxImagem.Top) / zoomAtual;
+
+            zoomAtual = novoZoom;
+            RedimensionarImagemExibida();
+            AtualizarIndicadorZoom();
+
+            if (!panelImagem.AutoScroll)
+            {
+                return;
+            }
+
+            int novoDeslocamentoX = (int)Math.Round((pontoImagemX * zoomAtual) + pictureBoxImagem.Left - pontoAncoragemPainel.X);
+            int novoDeslocamentoY = (int)Math.Round((pontoImagemY * zoomAtual) + pictureBoxImagem.Top - pontoAncoragemPainel.Y);
+            panelImagem.AutoScrollPosition = new Point(Math.Max(0, novoDeslocamentoX), Math.Max(0, novoDeslocamentoY));
+        }
+
+        private void RedimensionarImagemExibida(bool resetarScroll = false)
+        {
+            if (pictureBoxImagem.Image is null)
+            {
+                return;
+            }
+
+            int novaLargura = Math.Max(1, (int)Math.Round(pictureBoxImagem.Image.Width * zoomAtual));
+            int novaAltura = Math.Max(1, (int)Math.Round(pictureBoxImagem.Image.Height * zoomAtual));
+
+            pictureBoxImagem.Size = new Size(novaLargura, novaAltura);
+            PosicionarImagemNoPainel(resetarScroll);
+        }
+
+        private void PosicionarImagemNoPainel(bool resetarScroll)
+        {
+            if (pictureBoxImagem.Image is null)
+            {
+                return;
+            }
+
+            bool cabeHorizontal = pictureBoxImagem.Width <= panelImagem.ClientSize.Width;
+            bool cabeVertical = pictureBoxImagem.Height <= panelImagem.ClientSize.Height;
+            bool deveCentralizar = centralizarImagemAtiva && cabeHorizontal && cabeVertical;
+
+            if (deveCentralizar)
+            {
+                panelImagem.AutoScroll = false;
+                int posicaoX = (panelImagem.ClientSize.Width - pictureBoxImagem.Width) / 2;
+                int posicaoY = (panelImagem.ClientSize.Height - pictureBoxImagem.Height) / 2;
+                pictureBoxImagem.Location = new Point(Math.Max(0, posicaoX), Math.Max(0, posicaoY));
+                return;
+            }
+
+            bool estavaSemScroll = !panelImagem.AutoScroll;
+            panelImagem.AutoScroll = true;
+            pictureBoxImagem.Location = Point.Empty;
+            panelImagem.AutoScrollMinSize = pictureBoxImagem.Size;
+
+            if (resetarScroll || estavaSemScroll)
+            {
+                if (centralizarImagemAtiva)
+                {
+                    int centroX = Math.Max(0, (pictureBoxImagem.Width - panelImagem.ClientSize.Width) / 2);
+                    int centroY = Math.Max(0, (pictureBoxImagem.Height - panelImagem.ClientSize.Height) / 2);
+                    panelImagem.AutoScrollPosition = new Point(centroX, centroY);
+                }
+                else
+                {
+                    panelImagem.AutoScrollPosition = new Point(0, 0);
+                }
+            }
+        }
+
+        private void AtualizarIndicadorZoom()
+        {
+            labelZoom.Text = $"{zoomAtual * 100f:0}%";
+        }
+
+        private void ConfigurarMenuContextoImagem()
+        {
+            menuContextoImagem = new ContextMenuStrip(components);
+
+            menuContextoZoom100 = new ToolStripMenuItem("Zoom 100%");
+            menuContextoZoom100.Click += (_, _) => AplicarZoom100();
+
+            menuContextoRotacionar = new ToolStripMenuItem("Rotacionar");
+            var menuContextoGirarDireita = new ToolStripMenuItem("90 graus horario");
+            menuContextoGirarDireita.Click += (_, _) => RotacionarImagem(RotateFlipType.Rotate90FlipNone);
+            var menuContextoGirarEsquerda = new ToolStripMenuItem("90 graus anti-horario");
+            menuContextoGirarEsquerda.Click += (_, _) => RotacionarImagem(RotateFlipType.Rotate270FlipNone);
+            menuContextoRotacionar.DropDownItems.AddRange(
+                new ToolStripItem[] { menuContextoGirarDireita, menuContextoGirarEsquerda });
+
+            menuContextoCantoSuperiorEsquerdo = new ToolStripMenuItem("Canto superior esquerdo");
+            menuContextoCantoSuperiorEsquerdo.Click += (_, _) => DefinirAlinhamentoCantoSuperiorEsquerdo();
+
+            menuContextoCentralizar = new ToolStripMenuItem("Centralizar no programa");
+            menuContextoCentralizar.Click += (_, _) => DefinirAlinhamentoCentralizado();
+
+            menuContextoImagem.Items.AddRange(
+                new ToolStripItem[]
+                {
+                    menuContextoZoom100,
+                    menuContextoRotacionar,
+                    new ToolStripSeparator(),
+                    menuContextoCantoSuperiorEsquerdo,
+                    menuContextoCentralizar
+                });
+
+            menuContextoImagem.Opening += menuContextoImagem_Opening;
+            pictureBoxImagem.ContextMenuStrip = menuContextoImagem;
+            panelImagem.ContextMenuStrip = menuContextoImagem;
+        }
+
+        private void menuContextoImagem_Opening(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            bool possuiImagem = pictureBoxImagem.Image is not null;
+
+            if (menuContextoZoom100 is not null)
+            {
+                menuContextoZoom100.Enabled = possuiImagem;
+            }
+
+            if (menuContextoRotacionar is not null)
+            {
+                menuContextoRotacionar.Enabled = possuiImagem;
+            }
+
+            if (menuContextoCantoSuperiorEsquerdo is not null)
+            {
+                menuContextoCantoSuperiorEsquerdo.Enabled = possuiImagem;
+                menuContextoCantoSuperiorEsquerdo.Checked = possuiImagem && !centralizarImagemAtiva;
+            }
+
+            if (menuContextoCentralizar is not null)
+            {
+                menuContextoCentralizar.Enabled = possuiImagem;
+                menuContextoCentralizar.Checked = possuiImagem && centralizarImagemAtiva;
+            }
+        }
+
+        private void AplicarZoom100()
+        {
+            if (pictureBoxImagem.Image is null)
+            {
+                return;
+            }
+
+            zoomAtual = 1f;
+            RedimensionarImagemExibida(resetarScroll: true);
+            AtualizarIndicadorZoom();
+        }
+
+        private void RotacionarImagem(RotateFlipType tipoRotacao)
+        {
+            if (pictureBoxImagem.Image is null)
+            {
+                return;
+            }
+
+            float zoomAnterior = zoomAtual;
+
+            using var bitmapTemporario = new Bitmap(pictureBoxImagem.Image);
+            bitmapTemporario.RotateFlip(tipoRotacao);
+            var bitmapRotacionado = new Bitmap(bitmapTemporario);
+
+            var imagemAnterior = pictureBoxImagem.Image;
+            pictureBoxImagem.Image = bitmapRotacionado;
+            imagemAnterior?.Dispose();
+
+            zoomAtual = Math.Clamp(zoomAnterior, ZoomMinimo, ZoomMaximo);
+            RedimensionarImagemExibida(resetarScroll: true);
+            AtualizarIndicadorZoom();
+        }
+
+        private void DefinirAlinhamentoCantoSuperiorEsquerdo()
+        {
+            if (centralizarImagemToolStripMenuItem.Checked)
+            {
+                centralizarImagemToolStripMenuItem.Checked = false;
+                return;
+            }
+
+            centralizarImagemAtiva = false;
+            PosicionarImagemNoPainel(resetarScroll: true);
+        }
+
+        private void DefinirAlinhamentoCentralizado()
+        {
+            if (!centralizarImagemToolStripMenuItem.Checked)
+            {
+                centralizarImagemToolStripMenuItem.Checked = true;
+                return;
+            }
+
+            centralizarImagemAtiva = true;
+            PosicionarImagemNoPainel(resetarScroll: true);
+        }
+
+        private void centralizarImagemToolStripMenuItem_CheckedChanged(object? sender, EventArgs e)
+        {
+            centralizarImagemAtiva = centralizarImagemToolStripMenuItem.Checked;
+            PosicionarImagemNoPainel(resetarScroll: true);
+        }
+
+        private void panelImagem_Resize(object? sender, EventArgs e)
+        {
+            PosicionarImagemNoPainel(resetarScroll: false);
+        }
+
+        private void AtualizarListaNavegacao(string caminhoArquivoAtual)
+        {
+            string? pasta = Path.GetDirectoryName(caminhoArquivoAtual);
+            if (string.IsNullOrWhiteSpace(pasta) || !Directory.Exists(pasta))
+            {
+                imagensDaPasta.Clear();
+                indiceImagemAtual = -1;
+                return;
+            }
+
+            imagensDaPasta = Directory
+                .GetFiles(pasta)
+                .Where(EhArquivoNavegavel)
+                .OrderBy(Path.GetFileName, StringComparer.CurrentCultureIgnoreCase)
+                .ToList();
+
+            if (!imagensDaPasta.Contains(caminhoArquivoAtual, StringComparer.OrdinalIgnoreCase))
+            {
+                imagensDaPasta.Add(caminhoArquivoAtual);
+                imagensDaPasta = imagensDaPasta
+                    .OrderBy(Path.GetFileName, StringComparer.CurrentCultureIgnoreCase)
+                    .ToList();
+            }
+
+            indiceImagemAtual = imagensDaPasta.FindIndex(caminho =>
+                string.Equals(caminho, caminhoArquivoAtual, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private static bool EhArquivoNavegavel(string caminhoArquivo)
+        {
+            string extensao = Path.GetExtension(caminhoArquivo);
+            return ExtensoesNavegaveis.Contains(extensao, StringComparer.OrdinalIgnoreCase);
+        }
+
+        private void NavegarImagem(int deslocamento)
+        {
+            if (indiceImagemAtual < 0 || imagensDaPasta.Count == 0)
+            {
+                return;
+            }
+
+            int novoIndice = indiceImagemAtual + deslocamento;
+            if (novoIndice < 0 || novoIndice >= imagensDaPasta.Count)
+            {
+                return;
+            }
+
+            CarregarImagemTga(imagensDaPasta[novoIndice]);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Left)
+            {
+                NavegarImagem(-1);
+                return true;
+            }
+
+            if (keyData == Keys.Right)
+            {
+                NavegarImagem(1);
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            pictureBoxImagem.Image?.Dispose();
+            base.OnFormClosed(e);
+        }
+    }
+}
